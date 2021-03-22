68c8180ce7e9008562f85a9290bbcb90b194ab99
Manuel Rubio <manuel.rubio.arias@gmail.com>
2021-03-22 12:13:54 +0100

    mysql init

---
+++ srcs/mysql/srcs/start.sh
@@ -0,0 +1,1 @@
+mysql_install_db --user=mysql --ldata=/var/lib/mysql
--- start.sh
+++ start.sh
@@ -6,11 +6,11 @@ B=$'\e[34m'
 
 clear
 
-echo "${G}Deleting old MINIKUBE${END}"
+echo "${B}Deleting old MINIKUBE${END}"
 minikube stop
 minikube delete
 
-echo "${G}Starting MINIKUBE${END}"
+echo "${B}Starting MINIKUBE${END}"
 minikube start --vm-driver=virtualbox
 
 eval $(minikube docker-env)
@@ -20,12 +20,25 @@ minikube addons enable metallb
 kubectl apply -f ./srcs/metallb/metallb.yaml
 
 #Building images
-echo "${G}Creating docker images${END}"
+echo "${B}Creating docker images${END}"
 docker build --quiet ./srcs/nginx -t my-nginx #-----------NGINX
 
 #Deploying images
-echo "${G}Deploying images into pods${END}"
+echo "${B}Deploying images into pods${END}"
 kubectl apply -f ./srcs/nginx/nginx.yaml
 kubectl expose deployment nginx --name=nginx --type=LoadBalancer
+
+echo "${B}NGINX IP:${END} ${G}192.168.99.100:80${END}"
+echo "${B}MYSQL IP:${END} ${G}0.0.0.0:${END}"
+echo "${B}PHPMYADMIN IP:${END} ${G}0.0.0.0:${END}"
+echo "${B}WORDPRESS IP:${END} ${G}0.0.0.0:${END}"
+echo "${B}GRAFANA IP:${END} ${G}0.0.0.0:${END}"
+echo " "
+echo "·------------------------------------------·"
+echo "| MySQL | PhpMyAdmin | Wordpress | Grafana |"
+echo "|------------------------------------------|"
+echo "| mysql | phpmyadmin | wordpress | grafana |"
+echo "| pw123 |   pw123    |   pw123   |  pw123  |"
+echo "·------------------------------------------·"
 
 minikube dashboard
